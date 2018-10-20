f528eea0478f32b17d431fa84bf88bd6f0423800
VidyaniGunarathne <44308672+VidyaniGunarathne@users.noreply.github.com>
2018-10-20 10:04:09 +0530

    aa

---
+++ OrganizationalChart.java
@@ -0,0 +1,24 @@
+// Don't place your source in a package
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// Please name your class Main
+class Main{}
+class Engineering{
+    Scanner user1 = new Scanner(System.in);
+    System.out.println("engineering company");
+    int noEmployees= 5;
+    int noDivisions=30;
+    
+}
+
+
+/*class Organization extends * {
+	public static void main (String[] args) {
+	    Organization no=new Organization();
+	    System.out.println(no.noEmployees);
+	    System.out.println(no.noDivisions);
+	    
+	}
+}*/

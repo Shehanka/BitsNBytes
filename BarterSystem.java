67b84deb859bce07ec39c19d7a9d0ea09048c620
Shehanka <hcsperera@outlook.com>
2018-10-20 11:35:09 +0530

    Pac Man

--- BarterSystem.java
+++ BarterSystem.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+
 /**
  * @author chamodshehanka on 10/20/2018
  * @project BitsNBytes
  **/
 public class BarterSystem {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
 
     }
 }
